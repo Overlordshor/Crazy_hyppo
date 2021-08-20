72f8a8fdbf6e4abcd6d9a2dff8632cf97253d498
DmitrySharov <d.sharov@gamesredrift.com>
2021-08-20 23:49:48 +0300

    feat: Player

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,23 @@
+﻿using Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Handler
+{
+	public class ClickHandler : MonoBehaviour
+	{
+		[SerializeField] private Player _player;
+
+		private void OnMouseDown()
+		{
+			_player.Rotate();
+		}
+
+		private void OnMouseUp()
+		{
+			_player.Launch();
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Game
+{
+	public class Player : MonoBehaviour
+	{
+		[SerializeField] private float _rotationDuration;
+		[SerializeField] private float _launchPower;
+		[SerializeField] private float _launchDuration;
+
+		private Tween _rotateTween;
+		private Tween _lanuchTween;
+		private Vector3 _startPosition;
+
+		private void Awake()
+		{
+			_startPosition = transform.position;
+
+			_rotateTween = transform.DORotate(new Vector3(0, 360, 0), _rotationDuration, RotateMode.FastBeyond360)
+						.SetEase(Ease.Linear)
+						.SetLoops(-1)
+						.Pause();
+		}
+
+		public void Launch()
+		{
+			_rotateTween.Pause();
+			_lanuchTween = transform.DOMove(transform.forward * _launchPower, 1f)
+				.OnComplete(() => ResetToStartPosition());
+		}
+
+		public void Rotate()
+		{
+			_rotateTween.Play();
+		}
+
+		private void ResetToStartPosition()
+		{
+			transform.position = _startPosition;
+		}
+
+		private void OnCollisionEnter(Collision collision)
+		{
+			if (!collision.gameObject.TryGetComponent<Enemy>(out var enemy))
+				return;
+
+			enemy.ApplyDamage();
+			Debug.Log("HIT");
+
+			_lanuchTween.Kill();
+			ResetToStartPosition();
+		}
+	}
+}
