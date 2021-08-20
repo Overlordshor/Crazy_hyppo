165547b50cfbf55c3e5cd56901b1306264f5e163
DmitrySharov <d.sharov@gamesredrift.com>
2021-08-20 23:50:06 +0300

    feat: Enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Game
+{
+	public class Enemy : MonoBehaviour
+	{
+		private Tween tween;
+
+		private void Awake()
+		{
+			tween = transform.DOMove(FindObjectOfType<Lake>().Position, 10f)
+				.SetEase(Ease.Linear);
+		}
+
+		public void ApplyDamage()
+		{
+			Die();
+		}
+
+		private void Die()
+		{
+			Debug.Log("Enemy is dead");
+			Destroy(gameObject);
+		}
+
+		private void OnDestroy()
+		{
+			tween?.Kill();
+		}
+	}
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+	public class EnemySpawner : MonoBehaviour
+	{
+		[SerializeField] private Enemy _enemy = default;
+		[SerializeField] private float spawnDelay = default;
+		[SerializeField] private List<Transform> spawnPositions = default;
+
+		private void Awake()
+		{
+			DOVirtual.DelayedCall(spawnDelay, () => Spawn())
+				.SetLoops(-1);
+		}
+
+		private void Spawn()
+		{
+			Instantiate(_enemy, GetRandomSpawnPosition(), Quaternion.identity, transform).gameObject
+				.SetActive(true);
+		}
+
+		private Vector3 GetRandomSpawnPosition()
+		{
+			var randomIndex = Random.Range(0, spawnPositions.Count);
+			return spawnPositions[randomIndex].position;
+		}
+	}
+}
