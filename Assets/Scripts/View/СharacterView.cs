a094050eb0504c10964eabf6d04bc4057309ea89
Dmitry Sharov <overlord.shor@gmail.com>
2021-08-21 11:28:50 +0300

    feat: СharacterView

---
+++ Assets/Scripts/View/СharacterView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Game.Сharacter.View
+{
+	public class СharacterView : MonoBehaviour
+	{
+		[SerializeField] private Sprite _sprite;
+		[SerializeField] private SpriteRenderer _view;
+
+		private void Awake()
+		{
+			_view.sprite = _sprite;
+		}
+	}
+}
