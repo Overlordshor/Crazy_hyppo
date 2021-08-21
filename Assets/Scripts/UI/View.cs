655a110ae91be6e4f055ced8f934ce5088b18442
Dmitry Sharov <overlord.shor@gmail.com>
2021-08-21 21:57:13 +0300

    feat: intargate LeanWindow

--- Assets/Scripts/UI/View.cs
+++ Assets/Scripts/UI/View.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Lean.Gui;
 using UnityEngine;
 
 namespace UI
@@ -7,18 +7,46 @@ namespace UI
 	{
 		[SerializeField] private bool isStart = default;
 
+		protected LeanWindow _leanWindow;
+
 		private void Awake()
 		{
-			gameObject.SetActive(isStart);
+			TryGetComponent<LeanWindow>(out _leanWindow);
+
+			if (_leanWindow == null)
+				gameObject.SetActive(isStart);
+			else
+				LeanWindowSetActive(isStart);
+
 			OnAwake();
+		}
+
+		private void LeanWindowSetActive(bool isStart)
+		{
+			if (isStart)
+				_leanWindow.TurnOn();
+			else
+				_leanWindow.TurnOff();
 		}
 
 		protected virtual void OnAwake()
 		{
 		}
 
-		public void Show() => gameObject.SetActive(true);
+		public void Show()
+		{
+			if (_leanWindow == null)
+				gameObject.SetActive(true);
+			else
+				_leanWindow.TurnOn();
+		}
 
-		protected void Close() => gameObject.SetActive(false);
+		protected void Close()
+		{
+			if (_leanWindow == null)
+				gameObject.SetActive(false);
+			else
+				_leanWindow.TurnOff();
+		}
 	}
 }
